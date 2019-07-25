aa993c6fc6c02da00973a99ffbd8648e6e852acf
Innocent Lou <bisonlou@gmail.com>
2019-07-25 14:54:28 +0300

    fix calendar selected date contrast (#58)
    • Change the color on-hover to white

--- src/components/sell/salesHistory/searchPopper.js
+++ src/components/sell/salesHistory/searchPopper.js
@@ -1,14 +1,38 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import {
-  Grid, Grow, Paper, Popper, Button, TextField,
+  Grid, Grow, Paper, Popper, Button, TextField, createMuiTheme
 } from '@material-ui/core';
 import DateFnsUtils from '@date-io/date-fns'; // import
 import { InlineDatePicker, MuiPickersUtilsProvider } from 'material-ui-pickers';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import '../../../assets/styles/salesHistory/dateTimePopper.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import { dateTimeStyles } from '../../../assets/styles/salesHistory/salesHistoryStyles';
+
+
+const calendarTheme = createMuiTheme({
+  overrides: {
+    MuiPickersDay: {
+      day: {
+        color: '#424242',
+      },
+      isSelected: {
+        backgroundColor: '#FAF33E',
+        color: '#424242',
+        '&:hover': {
+          color: '#FFFFFF',
+          backgroundColor: '#424242',
+        }
+      },
+      current: {
+        color: '#FAF33E',
+      },
+    },
+  },
+});
+
 
 export const SearchPopper = (props) => {
   const {
@@ -40,33 +64,35 @@ export const SearchPopper = (props) => {
             <Paper className={classes.searchPaper}>
               <Grid container className={classes.timeGrid}>
                 <Grid item container xs={12}>
-                  <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                    <InlineDatePicker
-                      id="search-datepicker"
-                      onlyCalendar
-                      keyboard
-                      clearable
-                      fullWidth
-                      disableFuture
-                      label="From"
-                      value={searchValues.from}
-                      onChange={event => handleSearchChange('from', event)}
-                      format="dd/MM/yyyy"
-                      className={classes.FormControl}
-                    />
-                    <InlineDatePicker
-                      onlyCalendar
-                      keyboard
-                      clearable
-                      fullWidth
-                      disableFuture
-                      label="To"
-                      value={searchValues.to}
-                      onChange={event => handleSearchChange('to', event)}
-                      format="dd/MM/yyyy"
-                      className={classes.FormControl}
-                    />
-                  </MuiPickersUtilsProvider>
+                  <MuiThemeProvider theme={calendarTheme}>
+                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                      <InlineDatePicker
+                        id="search-datepicker"
+                        onlyCalendar
+                        keyboard
+                        clearable
+                        fullWidth
+                        disableFuture
+                        label="From"
+                        value={searchValues.from}
+                        onChange={event => handleSearchChange('from', event)}
+                        format="dd/MM/yyyy"
+                        className={classes.FormControl}
+                      />
+                      <InlineDatePicker
+                        onlyCalendar
+                        keyboard
+                        clearable
+                        fullWidth
+                        disableFuture
+                        label="To"
+                        value={searchValues.to}
+                        onChange={event => handleSearchChange('to', event)}
+                        format="dd/MM/yyyy"
+                        className={classes.FormControl}
+                      />
+                    </MuiPickersUtilsProvider>
+                  </MuiThemeProvider>
                   <TextField
                     id="search-textfield"
                     select
