bfbbbbc0d2335e0f77c0252407e7fdf5af1236d7
innocent lou <bisonlou@gmail.com>
2019-08-18 16:11:14 +0300

    Fix population of dropdown boxes before page render

---
+++ src/queries/productsSuppliersCategoriesQuery.js
@@ -0,0 +1,30 @@
+import gql from 'graphql-tag';
+
+const GET_PRODUCTS_SUPPLIERS_CATEGORIES = gql`
+query ($outletId: Int!){
+  approvedSuppliers{
+  id,
+  name
+  }
+  productCategories(
+    outletId: $outletId,
+    ){
+    id
+    name
+    loyaltyWeight
+    isVatApplicable
+    markup
+  }
+  measurementUnit{
+    id,
+    name
+  }
+  products{
+  id,
+  productName,
+  isApproved
+  }
+}
+`;
+
+export default GET_PRODUCTS_SUPPLIERS_CATEGORIES;
